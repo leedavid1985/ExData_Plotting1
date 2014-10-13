e19176425e15b32924dd0050c2157bdce08bccf6
leedavid1985 <crim.lee@gmail.com>
2014-10-12 23:35:51 -0400

    final

--- plot4.R
+++ plot4.R
@@ -7,19 +7,30 @@ mydata2 <- mydata
 mydata2$Date <- as.character(mydata2$Date)
 onlythese <-subset(mydata2,Date=="1/2/2007" | Date=="2/2/2007")
 
-##Combines date and time for Plot3
+##Combines for Plot2
 onlythese$datetime <- as.POSIXct(paste(onlythese$Date, onlythese$Time), format="%d/%m/%Y %H:%M:%S")
 onlythese$Global_active_power <- as.character(onlythese$Global_active_power)
 onlythese$Global_active_power <- as.numeric(onlythese$Global_active_power)  ##Makes readable
+
+##Combines for Plot3
 onlythese$Sub_metering_1 <- as.numeric(onlythese$Sub_metering_1)
 onlythese$Sub_metering_2 <- as.numeric(onlythese$Sub_metering_2)
 onlythese$Sub_metering_3 <- as.numeric(onlythese$Sub_metering_3)
 
-##Make Plot3
-png(file="plot3.png") ##Opens png device before plotting
+##Combines for Plot4
+onlythese$Global_reactive_power <- as.character(onlythese$Global_reactive_power)
+onlythese$Global_reactive_power <- as.numeric(onlythese$Global_reactive_power)  ##Makes readable
+
+##Make Plot4
+png(file="plot4.png") ##Opens png device before plotting
+par(mfrow=c(2,2))
+mtext("",outer=FALSE)
+plot.ts(onlythese$datetime, onlythese$Global_active_power,type="l", ylab="Global Active Power (kilowatts)",xlab="") 
+plot(onlythese$datetime,onlythese$Voltage,type="l",ylab="Voltage",xlab="datetime")
 yrange <- range(c(onlythese$Sub_metering_1,onlythese$Sub_metering_2,onlythese$Sub_metering3))
 plot(onlythese$datetime, onlythese$Sub_metering_1,type="l", ylab="Global Active Power (kilowatts)",xlab="",ylim=yrange) 
 lines(onlythese$datetime,onlythese$Sub_metering_2,type="l",col="red")
 lines(onlythese$datetime,onlythese$Sub_metering_3,type="l",col="blue")
 legend("topright",lwd=2,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"))
+plot(onlythese$datetime,onlythese$Global_reactive_power,type="l",ylab="Global_reactive_power",xlab="datetime")
 dev.off()
